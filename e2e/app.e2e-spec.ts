032aa4a8f0e0c24adf138ca4019bb63cdba0894a
Nir Oren <nir.oren@cellebrite.com>
2018-04-07 16:36:07 +0300

    add tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { element, by, ElementFinder, ElementArrayFinder, browser, ExpectedConditions, promise } from 'protractor';
 
 describe('tic-tac-toe App', () => {
   let page: AppPage;
@@ -7,8 +8,43 @@ describe('tic-tac-toe App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('X should win', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    // expect(element(by.css('app-board'))).toBeDefined();
+    const board: ElementFinder = element(by.css('app-board'));
+    // const score: ElementFinder = element(by.tagName('board-main'));
+    const cubes: ElementArrayFinder = board.all(by.tagName('app-cube'));
+    cubes.get(0).click();
+    cubes.get(1).click();
+    cubes.get(4).click();
+    cubes.get(2).click();
+    cubes.get(8).click();
+
+    // element(by.css('result')).then((text) => {
+    //   console.log("this is the element", text);
+    // });
+
+    // browser.wait(by.css('result')).then((isPresent) => {
+    //   console.log("result was found UUUUUUUUUUUUUu", isPresent);
+    // });
+    // const result: ElementFinder = element(by.css('result'));
+    // if (result) {
+    //   console.log("result was foundFFFFFFFFFFFFFFFFF!", result.getTagName());
+
+    //   result.getInnerHtml().then((text) => {
+    //     console.log(text);
+    //   });
+    // }
+
+    waitForClass('result').then(() => {
+      element(by.className('result')).getText().then((text) => {
+        expect(text).toContain('X is the Winner');
+      });
+    });
+
   });
 });
+
+export function waitForClass(className: string): promise.Promise<{}> {
+  return browser.wait(ExpectedConditions.visibilityOf(element.all(by.className(className)).first()), 1000);
+}
--- src/app/BL/game.spec.ts
+++ src/app/BL/game.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { Game } from './game';
+import { IResult } from '../model/result';
+import { GAME_STATE } from '../model/game-state';
 
 
-describe('Game', () => {
+fdescribe('Game', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [Game]
@@ -13,8 +15,29 @@ describe('Game', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('X should be a winner', inject([Game], (service: Game) => {
+  it('X should win diagonal', inject([Game], (service: Game) => {
+    let result: IResult;
     service.start();
-    // service.playTurn()
+    service.playTurn(0); // X
+    service.playTurn(1); // O
+    service.playTurn(4); // X
+    service.playTurn(2); // O
+    result = service.playTurn(8); // X
+    expect(result.state).toEqual(GAME_STATE.WIN);
+  }));
+
+  it('Result should be a draw', inject([Game], (service: Game) => {
+    let result: IResult;
+    service.start();
+    service.playTurn(0); // X
+    service.playTurn(1); // O
+    service.playTurn(2); // X
+    service.playTurn(3); // O
+    service.playTurn(5); // X
+    service.playTurn(4); // O
+    service.playTurn(6); // X
+    service.playTurn(8); // O
+    result = service.playTurn(7); // X
+    expect(result.state).toEqual(GAME_STATE.DRAW);
   }));
 });
